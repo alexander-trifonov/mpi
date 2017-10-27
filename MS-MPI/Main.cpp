94bfd7d4ff5bb2cd46cd01e1c7b2be42da6d3424
Alexander Trifonov <mobious96@gmail.com>
2017-10-27 10:58:22 +0300

    FINAL FINAL OF FINAL

--- MS-MPI/Main.cpp
+++ MS-MPI/Main.cpp
@@ -1,127 +1,127 @@
-#include "mpi.h"
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-using namespace std;
-
-int GenerateMatrix(double** &matrix, int rows, int cols, int min_range, int max_range)
-{
-	srand(time(NULL));
-	/*
-	matrix = (double*)malloc(cols*rows*sizeof(double));
-	for (int i = 0; i < cols*rows; i++)
-	{
-		matrix[i] = rand() % max_range + min_range;
-	}
-	*/
-	matrix = new double*[rows];
-	for (int i = 0; i < rows; i++)
-	{
-		matrix[i] = new double[cols];
-		for(int j = 0; j < cols; j++)
-			matrix[i][j] = rand() % max_range + min_range;
-	}
-	return 0;
-}
-
-void PrintMatrix(double** &matrix, int rows, int cols)
-{
-	/*
-	for (int i = 0; i < cols; i++)
-	{
-		for (int j = 0; j < rows; j++)
-			cout << matrix[j + (cols*i)] << " ";
-		cout << endl;
-	}
-	*/
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			cout << matrix[i][j] << " ";
-		}
-		cout << "\n";
-	}
-}
-
-void main3(int argc, char* argv[])
-{
-	MPI_Init(&argc, &argv);
-	int rank, RankSize;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &RankSize);
-	double** matrix;
-	int Rows = 6;
-	int Cols = 6;
-	if (rank == 0)
-	{
-		GenerateMatrix(matrix, Rows, Cols, 0, 10);
-		PrintMatrix(matrix, Rows, Cols);
-
-		if (RankSize > 1)
-		{
-			//Create data blocks for future transport
-			int odd = 0; //If odd, then add one row to main process
-			int RowsInSingleBlock = Rows / RankSize;
-			cout << "RowsInSingleBlock: " << RowsInSingleBlock << endl;
-			if (RowsInSingleBlock % 2 != 0)
-				odd = 1;
-			RowsInSingleBlock += odd;
-
-			double* Data_Buf;
-			for (int i = 1; i < RankSize; i++)
-			{
-				//double* Data_Buf_Cont = (double*)malloc(RowsInSingleBlock * Cols * sizeof(double));
-				Data_Buf = (double*)malloc(RowsInSingleBlock * Cols * sizeof(double*)); //may be 1 or more rows per data block for transfer
-				for (int j = 0; j < RowsInSingleBlock; j++)
-				{
-					for (int k = 0; k < Cols; k++)
-					{
-						Data_Buf[k + j*Cols] = matrix[j+(i-1)*RowsInSingleBlock][k];
-						cout << "Data_Buf["<< k + j*Cols <<"]: " << Data_Buf[k + j*Cols] << endl;
-					}
-				}
-				cout << "Sending to.. " << i << endl;
-				MPI_Send(&Data_Buf[0], Cols*RowsInSingleBlock, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-				int b = 1;
-				//MPI_Send(&b, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-				cout << "Sedned to " << i << endl;
-			}
-		}
-		cin.get();
-		MPI_Finalize();
-	}
-
-	if (rank != 0)
-	{
-		double* Recv_Buf = (double*)malloc(1 * Cols * sizeof(double*));
-		int a;
-		cout << "I am rank: " << rank << endl;
-		MPI_Recv(&Recv_Buf[0], Cols*(Rows / RankSize), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
-		//MPI_Recv(&a, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
-		cout << "Rank: " << rank << " received: " << endl;
-		for(int i = 0; i < (Rows / RankSize) * Cols; i++)
-			cout << Recv_Buf[i] << " ";
-		//PrintMatrix(Recv_Buf, Rows, Rows / RankSize);
-		cout << "\n";
-	}
-
-		// Unfinished realization with pointers.
-		/*
-		//Unfinished realization with malloc and scatter
-		double* matrix;
-		int Rows = 3;
-		int Cols = 3;
-		double* data_buf = (double*)malloc(sizeof(double)*Cols);
-		if (rank == 0)
-		{
-		GenerateMatrix(matrix, Rows, Cols, 0, 10);
-		PrintMatrix(matrix, Rows, Cols);
-		if (RankSize > 1)
-		{
-		MPI_Scatter(matrix, Cols, MPI_DOUBLE, data_buf, Cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		}
-		}
-		*/
-	
-}
+//#include "mpi.h"
+//#include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
+//using namespace std;
+//
+//int GenerateMatrix(double** &matrix, int rows, int cols, int min_range, int max_range)
+//{
+//	srand(time(NULL));
+//	/*
+//	matrix = (double*)malloc(cols*rows*sizeof(double));
+//	for (int i = 0; i < cols*rows; i++)
+//	{
+//		matrix[i] = rand() % max_range + min_range;
+//	}
+//	*/
+//	matrix = new double*[rows];
+//	for (int i = 0; i < rows; i++)
+//	{
+//		matrix[i] = new double[cols];
+//		for(int j = 0; j < cols; j++)
+//			matrix[i][j] = rand() % max_range + min_range;
+//	}
+//	return 0;
+//}
+//
+//void PrintMatrix(double** &matrix, int rows, int cols)
+//{
+//	/*
+//	for (int i = 0; i < cols; i++)
+//	{
+//		for (int j = 0; j < rows; j++)
+//			cout << matrix[j + (cols*i)] << " ";
+//		cout << endl;
+//	}
+//	*/
+//	for (int i = 0; i < rows; i++)
+//	{
+//		for (int j = 0; j < cols; j++)
+//		{
+//			cout << matrix[i][j] << " ";
+//		}
+//		cout << "\n";
+//	}
+//}
+//
+//void main3(int argc, char* argv[])
+//{
+//	MPI_Init(&argc, &argv);
+//	int rank, RankSize;
+//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//	MPI_Comm_size(MPI_COMM_WORLD, &RankSize);
+//	double** matrix;
+//	int Rows = 6;
+//	int Cols = 6;
+//	if (rank == 0)
+//	{
+//		GenerateMatrix(matrix, Rows, Cols, 0, 10);
+//		PrintMatrix(matrix, Rows, Cols);
+//
+//		if (RankSize > 1)
+//		{
+//			//Create data blocks for future transport
+//			int odd = 0; //If odd, then add one row to main process
+//			int RowsInSingleBlock = Rows / RankSize;
+//			cout << "RowsInSingleBlock: " << RowsInSingleBlock << endl;
+//			if (RowsInSingleBlock % 2 != 0)
+//				odd = 1;
+//			RowsInSingleBlock += odd;
+//
+//			double* Data_Buf;
+//			for (int i = 1; i < RankSize; i++)
+//			{
+//				//double* Data_Buf_Cont = (double*)malloc(RowsInSingleBlock * Cols * sizeof(double));
+//				Data_Buf = (double*)malloc(RowsInSingleBlock * Cols * sizeof(double*)); //may be 1 or more rows per data block for transfer
+//				for (int j = 0; j < RowsInSingleBlock; j++)
+//				{
+//					for (int k = 0; k < Cols; k++)
+//					{
+//						Data_Buf[k + j*Cols] = matrix[j+(i-1)*RowsInSingleBlock][k];
+//						cout << "Data_Buf["<< k + j*Cols <<"]: " << Data_Buf[k + j*Cols] << endl;
+//					}
+//				}
+//				cout << "Sending to.. " << i << endl;
+//				MPI_Send(&Data_Buf[0], Cols*RowsInSingleBlock, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+//				int b = 1;
+//				//MPI_Send(&b, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+//				cout << "Sedned to " << i << endl;
+//			}
+//		}
+//		cin.get();
+//		MPI_Finalize();
+//	}
+//
+//	if (rank != 0)
+//	{
+//		double* Recv_Buf = (double*)malloc(1 * Cols * sizeof(double*));
+//		int a;
+//		cout << "I am rank: " << rank << endl;
+//		MPI_Recv(&Recv_Buf[0], Cols*(Rows / RankSize), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUSES_IGNORE);
+//		//MPI_Recv(&a, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status);
+//		cout << "Rank: " << rank << " received: " << endl;
+//		for(int i = 0; i < (Rows / RankSize) * Cols; i++)
+//			cout << Recv_Buf[i] << " ";
+//		//PrintMatrix(Recv_Buf, Rows, Rows / RankSize);
+//		cout << "\n";
+//	}
+//
+//		// Unfinished realization with pointers.
+//		/*
+//		//Unfinished realization with malloc and scatter
+//		double* matrix;
+//		int Rows = 3;
+//		int Cols = 3;
+//		double* data_buf = (double*)malloc(sizeof(double)*Cols);
+//		if (rank == 0)
+//		{
+//		GenerateMatrix(matrix, Rows, Cols, 0, 10);
+//		PrintMatrix(matrix, Rows, Cols);
+//		if (RankSize > 1)
+//		{
+//		MPI_Scatter(matrix, Cols, MPI_DOUBLE, data_buf, Cols, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//		}
+//		}
+//		*/
+//	
+//}
--- MS-MPI/Source.cpp
+++ MS-MPI/Source.cpp
@@ -1,115 +1,131 @@
-#include "mpi.h"
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
-using namespace std;
-
-int GenerateMatrix(double* &matrix, int rows, int cols, int min_range, int max_range)
-{
-	srand(time(NULL));
-	matrix = (double*)malloc(cols*rows*sizeof(double));
-	for (int i = 0; i < cols*rows; i++)
-	{
-	matrix[i] = rand() % max_range + min_range;
-	cout << i/((rows*cols) / 100) <<"%" << '\r';
-	}
-	return 0;
-}
-
-void PrintMatrix(double* &matrix, int rows, int cols)
-{
-	for (int i = 0; i < cols; i++)
-	{
-	for (int j = 0; j < rows; j++)
-	cout << matrix[j + (cols*i)] << " ";
-	cout << endl;
-	}
-}
-
-double* FindMin(double* &matrix, int Rows, int Cols, int RankSize)
-{
-	double* min = (double*)malloc(Rows/RankSize * sizeof(double));
-	for (int i = 0; i < Rows/RankSize; i++)
-		min[i] = 65000;
-
-	for (int i = 0; i < (Rows / RankSize); i++)
-	{
-		for (int j = 0; j < Cols; j++)
-		{
-			if (matrix[j + i*Cols] < min[i])
-				min[i] = matrix[j + i*Cols];
-		}
-	}
-	return min;
-}
-
-void main(int argc, char* argv[])
-{
-	MPI_Init(&argc, &argv);
-	int rank, RankSize;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &RankSize);
-
-	
-	double* matrix;
-	int Rows = 12;
-	int Cols = Rows;
-	if (rank == 0)
-	{
-		cout << "[[ Generating matrix ]]" << endl;
-		cout << "[ Rows: " << Rows << " Cols: " << Cols << " ]" << endl;
-		GenerateMatrix(matrix, Rows, Cols, 0, 10);
-		if( Rows <= 12 )
-			PrintMatrix(matrix, Rows, Cols);
-	}
-	MPI_Barrier(MPI_COMM_WORLD);
-	double TimeStart;
-	double TimeEnd;
-	if (RankSize > 1)
-	{
-		double* min;
-		double* minRecv = (double*)malloc(Rows * sizeof(double));
-		if (rank == 0)
-		{
-			cout << "[[ MPI ]]" << endl;
-			for (int i = 1; i < RankSize; i++)
-			{
-				MPI_Send(&matrix[i*(Cols*(Rows / RankSize))], Cols*(Rows / RankSize), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
-				TimeStart = MPI_Wtime();
-				min = FindMin(matrix, Rows, Cols, RankSize);
-			}
-		}
-		else
-		{
-			double *RecvBuf = (double*)malloc(Cols*(Rows / RankSize) * sizeof(double));
-			MPI_Recv(RecvBuf, Cols*(Rows / RankSize), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			min = FindMin(RecvBuf, Rows, Cols, RankSize);
-			free(RecvBuf);
-		}
-		MPI_Gather(min, Rows / RankSize, MPI_DOUBLE, minRecv, Rows / RankSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		if (rank == 0)
-		{
-			TimeEnd = MPI_Wtime();
-			if (Rows <= 12)
-			{
-				cout << "Rank 0 received all mins: " << endl;
-				for (int i = 0; i < Rows; i++)
-				cout << i << ": " << minRecv[i] << endl;
-			}
-			cout << "Time: " << TimeEnd - TimeStart << endl;
-		}
-		free(minRecv);
-		free(min);
-
-		if (rank == 0)
-		{
-			cout << "[[ Sequential ]]" << endl;
-			TimeStart = MPI_Wtime();
-			min = FindMin(matrix, Rows, Cols, 1);
-			TimeEnd = MPI_Wtime();
-			cout << "Time: " << TimeEnd - TimeStart << endl;
-		}
-
-	}
-	MPI_Finalize();
-}
+//#include "mpi.h"
+//#include <iostream>
+//#include <stdlib.h>
+//#include <time.h>
+//using namespace std;
+//
+//int GenerateMatrix(double* &matrix, int rows, int cols, int min_range, int max_range)
+//{
+//	srand(time(NULL));
+//	matrix = (double*)malloc(cols*rows*sizeof(double));
+//	for (int i = 0; i < cols*rows; i++)
+//	{
+//	matrix[i] = rand() % max_range + min_range;
+//	//cout << i/((rows*cols) / 100) <<"%" << '\r';
+//	}
+//	return 0;
+//}
+//
+//void PrintMatrix(double* &matrix, int rows, int cols)
+//{
+//	for (int i = 0; i < cols; i++)
+//	{
+//	for (int j = 0; j < rows; j++)
+//	cout << matrix[j + (cols*i)] << " ";
+//	cout << endl;
+//	}
+//}
+//
+//double* FindMin(double* &matrix, int Rows, int Cols, int RankSize)
+//{
+//	double* min = (double*)malloc(Rows/RankSize * sizeof(double));
+//	for (int i = 0; i < Rows/RankSize; i++)
+//		min[i] = 65000;
+//
+//	for (int i = 0; i < (Rows / RankSize); i++)
+//	{
+//		for (int j = 0; j < Cols; j++)
+//		{
+//			if (matrix[j + i*Cols] < min[i])
+//				min[i] = matrix[j + i*Cols];
+//		}
+//	}
+//	return min;
+//}
+//
+//void main2123(int argc, char* argv[])
+//{
+//	MPI_Init(&argc, &argv);
+//	int rank, RankSize;
+//	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+//	MPI_Comm_size(MPI_COMM_WORLD, &RankSize);
+//
+//	
+//	double* matrix;
+//	int Rows = 6;
+//	int Cols = Rows;
+//	if (rank == 0)
+//	{
+//		cout << "[[ Generating matrix ]]" << endl;
+//		cout << "[ Rows: " << Rows << " Cols: " << Cols << " ]" << endl;
+//		GenerateMatrix(matrix, Rows, Cols, 0, 10);
+//		if (Rows <= 12)
+//			PrintMatrix(matrix, Rows, Cols);
+//
+//
+//	}
+//	MPI_Barrier(MPI_COMM_WORLD);
+//	double TimeStart;
+//	double TimeEnd;
+//	if (RankSize > 1)
+//	{
+//		double* min;
+//		double* minRecv = (double*)malloc(Rows * sizeof(double));
+//		if (rank == 0)
+//		{
+//			cout << "[[ MPI ]]" << endl;
+//			TimeStart = MPI_Wtime();
+//			for (int i = 1; i < RankSize; i++)
+//			{
+//				MPI_Send(&matrix[i*(Cols*(Rows / RankSize))], Cols*(Rows / RankSize), MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+//				
+//			}
+//
+//			min = FindMin(matrix, Rows, Cols, RankSize);
+//		}
+//		else
+//		{
+//			double *RecvBuf = (double*)malloc(Cols*(Rows / RankSize) * sizeof(double));
+//			MPI_Recv(RecvBuf, Cols*(Rows / RankSize), MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+//			min = FindMin(RecvBuf, Rows, Cols, RankSize);
+//			free(RecvBuf);
+//		}
+//		MPI_Gather(min, Rows / RankSize, MPI_DOUBLE, minRecv, Rows / RankSize, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+//		if (rank == 0)
+//		{
+//			TimeEnd = MPI_Wtime();
+//			if (Rows <= 12)
+//			{
+//				cout << "Rank 0 received all mins: " << endl;
+//				for (int i = 0; i < Rows; i++)
+//				cout << i << ": " << minRecv[i] << endl;
+//			}
+//			cout << "Time: " << TimeEnd - TimeStart << endl;
+//		}
+//		free(minRecv);
+//		free(min);
+//	}
+//
+//
+//	//
+//
+//	//функцию печати, больше функций.
+//	if(RankSize = 1)
+//		if (rank == 0)
+//		{
+//			cout << "[[ Sequential ]]" << endl;
+//			TimeStart = MPI_Wtime();
+//			double* min = FindMin(matrix, Rows, Cols, 1);
+//			TimeEnd = MPI_Wtime();
+//			if (Rows <= 12)
+//			{
+//				cout << "Rank 0 received all mins: " << endl;
+//				for (int i = 0; i < Rows; i++)
+//					cout << i << ": " << min[i] << endl;
+//			}
+//			cout << "Time: " << TimeEnd - TimeStart << endl;
+//			free(min);
+//		}
+//
+//	MPI_Finalize();
+//}
